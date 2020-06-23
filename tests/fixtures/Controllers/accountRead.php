72f3b4e12ece3c892be925ed290eaf181264da5a
Adam Kadlec <adam.kadlec@gmail.com>
2020-06-23 14:29:32 +0200

    Fixing tests after removed entity

--- tests/fixtures/Controllers/accountRead.php
+++ tests/fixtures/Controllers/accountRead.php
@@ -10,7 +10,7 @@ return [
 		__DIR__ . '/responses/account.read.json',
 	],
 	'included' => [
-		'/v1/me?include=emails,security-question',
+		'/v1/me?include=emails',
 		'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1NzU5MzIxMTgsImV4cCI6MTU3NTk1MzcxOCwianRpIjoiYWI5Y2MwNjktM2QzZS00Mzk3LWFmYjItNjY2ZDIyOTFlZWUwIn0.G507WDqVFp_mIaHE8eyg1LEywTDV1OGgWtYgFwoyCQg',
 		StatusCodeInterface::STATUS_OK,
 		__DIR__ . '/responses/account.read.included.json',
--- tests/fixtures/Controllers/accountRelationships.php
+++ tests/fixtures/Controllers/accountRelationships.php
@@ -3,31 +3,31 @@
 use Fig\Http\Message\StatusCodeInterface;
 
 return [
-	'emails'            => [
+	'emails'       => [
 		'/v1/me/relationships/emails',
 		'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1NzU5MzIxMTgsImV4cCI6MTU3NTk1MzcxOCwianRpIjoiYWI5Y2MwNjktM2QzZS00Mzk3LWFmYjItNjY2ZDIyOTFlZWUwIn0.G507WDqVFp_mIaHE8eyg1LEywTDV1OGgWtYgFwoyCQg',
 		StatusCodeInterface::STATUS_OK,
 		__DIR__ . '/responses/account.readRelationship.emails.json',
 	],
-	'identities'        => [
+	'identities'   => [
 		'/v1/me/relationships/identities',
 		'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1NzU5MzIxMTgsImV4cCI6MTU3NTk1MzcxOCwianRpIjoiYWI5Y2MwNjktM2QzZS00Mzk3LWFmYjItNjY2ZDIyOTFlZWUwIn0.G507WDqVFp_mIaHE8eyg1LEywTDV1OGgWtYgFwoyCQg',
 		StatusCodeInterface::STATUS_OK,
 		__DIR__ . '/responses/account.readRelationship.identities.json',
 	],
-	'roles'             => [
+	'roles'        => [
 		'/v1/me/relationships/roles',
 		'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1NzU5MzIxMTgsImV4cCI6MTU3NTk1MzcxOCwianRpIjoiYWI5Y2MwNjktM2QzZS00Mzk3LWFmYjItNjY2ZDIyOTFlZWUwIn0.G507WDqVFp_mIaHE8eyg1LEywTDV1OGgWtYgFwoyCQg',
 		StatusCodeInterface::STATUS_OK,
 		__DIR__ . '/responses/account.readRelationship.roles.json',
 	],
-	'unknown'           => [
+	'unknown'      => [
 		'/v1/me/relationships/unknown',
 		'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1NzU5MzIxMTgsImV4cCI6MTU3NTk1MzcxOCwianRpIjoiYWI5Y2MwNjktM2QzZS00Mzk3LWFmYjItNjY2ZDIyOTFlZWUwIn0.G507WDqVFp_mIaHE8eyg1LEywTDV1OGgWtYgFwoyCQg',
 		StatusCodeInterface::STATUS_NOT_FOUND,
 		__DIR__ . '/responses/account.readRelationship.unknown.json',
 	],
-	'invalidToken'      => [
+	'invalidToken' => [
 		'/v1/me/relationships/unknown',
 		'Bearer ayJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1NzU5MzIxMTgsImV4cCI6MTU3NTk1MzcxOCwianRpIjoiYWI5Y2MwNjktM2QzZS00Mzk3LWFmYjItNjY2ZDIyOTFlZWUwIn0.G507WDqVFp_mIaHE8eyg1LEywTDV1OGgWtYgFwoyCQg',
 		StatusCodeInterface::STATUS_UNAUTHORIZED,
