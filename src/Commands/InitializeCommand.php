0dbe22a388253e708c2fc6163f157bfc64fae210
Adam Kadlec <adam.kadlec@gmail.com>
2020-12-14 01:11:46 +0100

    Another cleanup

--- src/Commands/InitializeCommand.php
+++ src/Commands/InitializeCommand.php
@@ -221,7 +221,7 @@ class InitializeCommand extends Console\Command\Command
 			$accounts = $this->accountRepository->findAllBy($findAccounts);
 
 			if (count($accounts) === 0) {
-				$accountCmd = $symfonyApp->find('fb:auth-node:create:account');
+				$accountCmd = $symfonyApp->find('fb:auth-module:create:account');
 
 				$result = $accountCmd->run(new Input\ArrayInput([
 					'role'       => SimpleAuth\Constants::ROLE_ADMINISTRATOR,
--- tests/sql/vernemq.authentication.sql
+++
@@ -1,4 +0,0 @@
-INSERT IGNORE INTO `vmq_auth_acl` (`id`, `identity_id`, `mountpoint`, `client_id`, `username`, `password`,
-                                     `publish_acl`, `subscribe_acl`)
-VALUES (_binary 0x1A46CF0ACCE94AB58B7575E2965F2957, null, '', '',
-        'newDeviceUsername', 'adc471fdc75ccdc1cc5c7e3a9fb3ff704d40b0cf72c71fe832ba266a5ecff236', '[{"pattern":"/fb/newDeviceUsername/#"}]', '[{"pattern":"/fb/newDeviceUsername/#"}]');
